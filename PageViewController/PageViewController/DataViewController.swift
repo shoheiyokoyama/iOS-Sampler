04c95521f8db2a7e34c7f9f445a5c4005aebdc51
shoheiyokoyama <shohei.yok0602@gmail.com>
2016-06-30 17:13:09 +0900

    create pageviewController example

---
+++ PageViewController/PageViewController/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  PageViewController
+//
+//  Created by 横山祥平 on 2016/06/30.
+//  Copyright © 2016年 Shohei. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
