dc9dca0efdd54b304f644f6f4ae0366bd79cba02
shohei-yokoyama <shohei_yokoyama@cyberagent.co.jp>
2016-07-25 21:17:17 +0900

    遷移先のコントローラ作成

---
+++ CollectionView/CollectionView/viewer/BlickCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  BlickCollectionViewCell.swift
+//  CollectionView
+//
+//  Created by 横山 祥平 on 2016/07/25.
+//  Copyright © 2016年 Shohei Yokoyama. All rights reserved.
+//
+
+import UIKit
+
+class BlickCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- CollectionView/CollectionView/viewer/BlickViewController.swift
+++ CollectionView/CollectionView/viewer/BlickViewController.swift
@@ -8,28 +8,69 @@
 
 import UIKit
 
-class BlickViewController: UIViewController {
+final class BlickViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private let identifier = "BlickCollectionViewCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureCollectionView()
+        registerCell()
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+private extension BlickViewController {
+    private func configureCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .Vertical
+        
+//        layout.sectionInset = UIEdgeInsetsZero
+//        layout.minimumLineSpacing = 0
+//        layout.minimumInteritemSpacing = 0
+        collectionView.collectionViewLayout = layout
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func registerCell() {
+        let nib = UINib(nibName: identifier, bundle: nil)
+        collectionView.registerNib(nib, forCellWithReuseIdentifier: identifier)
     }
-    */
-
 }
+
+extension BlickViewController: UICollectionViewDelegate {
+    
+}
+
+extension BlickViewController: UICollectionViewDataSource {
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(identifier, forIndexPath: indexPath)
+        return cell
+    }
+}
+
+extension BlickViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let margin: CGFloat = 5
+        let height: CGFloat = 30
+        
+        let rowCount: CGFloat = 3
+        return CGSize(width: view.frame.width / rowCount - (margin * (rowCount - 1) / rowCount), height: height)
+    }
+    
+    
+}
+
