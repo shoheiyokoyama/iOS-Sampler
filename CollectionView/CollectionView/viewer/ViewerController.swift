6029cab6825f54aea4a6ff9074e0e07d7c8bedb6
shohei-yokoyama <shohei_yokoyama@cyberagent.co.jp>
2016-07-25 18:32:23 +0900

    viewerController作成

---
+++ CollectionView/CollectionView/viewer/ViewerController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewerController.swift
+//  CollectionView
+//
+//  Created by 横山 祥平 on 2016/07/25.
+//  Copyright © 2016年 Shohei Yokoyama. All rights reserved.
+//
+
+import UIKit
+
+final class ViewerController: UIViewController {
+    
+}
