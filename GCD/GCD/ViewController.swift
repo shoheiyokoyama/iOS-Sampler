5b986abf90f530b41fc56809aa5aa8a194236796
shohei-yokoyama <shohei.yok0602@gmail.com>
2016-11-14 19:15:42 +0900

    create UserNotification

--- GCD/GCD/ViewController.swift
+++ GCD/GCD/ViewController.swift
@@ -250,6 +250,8 @@ extension ViewController {
             }
         }
         
+        let a = OperationQueue
+        
         // excute without queue
         workItem.perform()
         
---
+++ UserNotifications/UserNotifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserNotifications
+//
+//  Created by 横山 祥平 on 2016/11/14.
+//  Copyright © 2016年 Shohei Yokoyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
