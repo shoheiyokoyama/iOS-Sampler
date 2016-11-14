df641fbc0bfd8de3122bc0afcdf137bb858b24f9
shohei-yokoyama <shohei.yok0602@gmail.com>
2016-11-14 19:28:10 +0900

    create project

---
+++ Notifications/Notifications/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notifications
+//
+//  Created by Shohei Yokoyama on 2016/11/14.
+//  Copyright © 2016年 Shohei Yokoyama. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
