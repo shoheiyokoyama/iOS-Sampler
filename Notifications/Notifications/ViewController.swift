450ce15536c39f29e84868c7f3f4d5b799f51f65
shohei-yokoyama <shohei.yok0602@gmail.com>
2016-11-15 11:19:23 +0900

    update and remove user notification

--- Notifications/Notifications/ViewController.swift
+++ Notifications/Notifications/ViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 import UserNotifications
 
 class ViewController: UIViewController {
+    
+    let userNotification  = UNUserNotificationCenter.current()
+    let notificationIdentifier = "localNotification"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // First time only
         confirmAuthorization()
+        configureUserNotification()
+        addLocalNotification()
+        
+        /*
+        updateUserNotificaton()
+        removeUserNotification()
+         */
+    }
+    
+    private func configureUserNotification() {
+        userNotification.delegate = self
     }
 }
 
@@ -27,10 +42,52 @@ private extension ViewController {
                 print(error)
                 return
             }
-            
             // Register notification
             UIApplication.shared.registerForRemoteNotifications()
         }
+    }
+}
+
+// MARK: - Local Notification
+
+extension ViewController {
+    func addLocalNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Title"
+        content.body  = "body"
+        content.sound = .default()
+        
+        // not notify when App active
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: "localNotification", content: content, trigger: trigger)
+        userNotification.add(request)
+    }
+}
+
+// MARK: - UNUserNotificationCenterDelegate -
+
+extension ViewController: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("User select notification in foreground")
+    }
+}
+
+// MARK: - Update and Remove -
+
+extension ViewController {
+    func removeUserNotification() {
+        userNotification.removeDeliveredNotifications(withIdentifiers: [notificationIdentifier])
+    }
+    
+    func updateUserNotificaton() {
+        let content = UNMutableNotificationContent()
+        content.title = "New Title"
+        content.body  = "New Body"
+        content.sound = .default()
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
+        let request = UNNotificationRequest(identifier: notificationIdentifier, content: content, trigger: trigger)
+        userNotification.add(request) { _ in }
     }
 }
 
