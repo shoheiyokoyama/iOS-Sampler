d89eabc85db2233b392b75c8e2f1475bbb42f1a6
shohei-yokoyama <shohei.yok0602@gmail.com>
2016-11-15 11:39:42 +0900

    custom notification

--- Notifications/Notifications/ViewController.swift
+++ Notifications/Notifications/ViewController.swift
@@ -12,19 +12,34 @@ import UserNotifications
 class ViewController: UIViewController {
     
     let userNotification  = UNUserNotificationCenter.current()
-    let notificationIdentifier = "localNotification"
+    
+    // UserNotificatonIdentifier
+    struct UNI {
+        static let local  = "localNotification"
+        static let custom = "customUserNotification"
+        static let category = "category-message"
+        
+        struct Action {
+            static let replay = "replay-action"
+            static let delete = "delete-action"
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // First time only
         confirmAuthorization()
         configureUserNotification()
-        addLocalNotification()
+        
+        //addLocalNotification()
         
         /*
         updateUserNotificaton()
         removeUserNotification()
          */
+        
+        registerCustomNotification()
+        addCustomNotification()
     }
     
     private func configureUserNotification() {
@@ -59,7 +74,7 @@ extension ViewController {
         
         // not notify when App active
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        let request = UNNotificationRequest(identifier: "localNotification", content: content, trigger: trigger)
+        let request = UNNotificationRequest(identifier: UNI.local, content: content, trigger: trigger)
         userNotification.add(request)
     }
 }
@@ -69,6 +84,14 @@ extension ViewController {
 extension ViewController: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         print("User select notification in foreground")
+        
+        if response.actionIdentifier == UNI.Action.replay {
+            print("Selected Reply Action")
+        }
+        if response.actionIdentifier == UNI.Action.delete {
+            print("Selected Delete Action")
+        }
+        completionHandler()
     }
 }
 
@@ -76,7 +99,7 @@ extension ViewController: UNUserNotificationCenterDelegate {
 
 extension ViewController {
     func removeUserNotification() {
-        userNotification.removeDeliveredNotifications(withIdentifiers: [notificationIdentifier])
+        userNotification.removeDeliveredNotifications(withIdentifiers: [UNI.local])
     }
     
     func updateUserNotificaton() {
@@ -86,8 +109,33 @@ extension ViewController {
         content.sound = .default()
         
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
-        let request = UNNotificationRequest(identifier: notificationIdentifier, content: content, trigger: trigger)
+        let request = UNNotificationRequest(identifier: UNI.local, content: content, trigger: trigger)
         userNotification.add(request) { _ in }
+    }
+}
+
+// MARK: - UNNotificationAction & Category
+
+extension ViewController {
+    func registerCustomNotification() {
+        let replayAction = UNNotificationAction(identifier: UNI.Action.replay, title: "replay", options: [])
+        let deleteAction = UNNotificationAction(identifier: UNI.Action.delete, title: "delete", options: [])
+        
+        let category = UNNotificationCategory(identifier: UNI.category, actions: [replayAction, deleteAction], intentIdentifiers: [], options: [])
+        userNotification.setNotificationCategories([category])
+    }
+    
+    func addCustomNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Custom Title"
+        content.body  = "Custom body"
+        content.sound = .default()
+        
+        content.categoryIdentifier = UNI.category
+        
+        let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: UNI.custom, content: content, trigger: trigger)
+        userNotification.add(request)
     }
 }
 
