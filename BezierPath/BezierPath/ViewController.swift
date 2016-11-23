5a949803b97329dba85c672ac4d9c4293302cc0f
shoheiyokoyama <shohei.yok0602@gmail.com>
2016-11-23 16:16:31 +0900

    define multiple semicircle

--- BezierPath/BezierPath/ViewController.swift
+++ BezierPath/BezierPath/ViewController.swift
@@ -8,24 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CircleType {
+    case circle, semicircle, harfSemicircle, leftSemicircle, rightSemicircle
+    
+    var parameter: (start: CGFloat, end: CGFloat, clockwise: Bool) {
+        let π = CGFloat(M_PI)
+        switch self {
+        case .circle: return (0, π * 2, false)
+        case .semicircle: return (0, π, false)
+        case .harfSemicircle: return (0, π + (π / 2), false)
+        case .leftSemicircle: return (π + (π / 2), π / 2, false)
+        case .rightSemicircle: return (π + (π / 2), π / 2, true)
+        }
+    }
+}
 
+class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        let parameter: (start: CGFloat, end: CGFloat)
-        parameter = (0, CGFloat(M_PI))
-        
-        drawCircle(startAngle: parameter.start, endAngle: parameter.end)
+        drawCircle(.rightSemicircle)
     }
 }
 
 extension ViewController {
-    func drawCircle(startAngle: CGFloat, endAngle: CGFloat, radius: CGFloat = 100) {
+    func drawCircle(_ type: CircleType) {
         let centerPoint = CGPoint(x: view.frame.width / 2, y: view.frame.height / 2)
         
         let path = UIBezierPath()
         path.move(to: centerPoint)
-        path.addArc(withCenter: centerPoint, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+        path.addArc(withCenter: centerPoint, radius: 100, startAngle: type.parameter.start, endAngle: type.parameter.end, clockwise: type.parameter.clockwise)
         
         let layer = CAShapeLayer()
         layer.fillColor = UIColor.orange.cgColor
