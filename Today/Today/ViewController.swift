13dc1df1687fd96200ce40de46fac20e1b09b3ac
shohei-yokoyama <shohei.yok0602@gmail.com>
2016-12-19 18:37:14 +0900

    create new target for Today Extension

--- Today/Today/ViewController.swift
+++ Today/Today/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Today
 //
-//  Created by 横山 祥平 on 2016/12/19.
+//  Created by Shohei Yokoyama on 2016/12/19.
 //  Copyright © 2016年 com.shoheiyokoyama. All rights reserved.
 //
 
@@ -12,14 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
