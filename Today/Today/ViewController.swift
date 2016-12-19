a4303e2fd92c6382c1fa1b6d98f46fcd53aac38b
shohei-yokoyama <shohei.yok0602@gmail.com>
2016-12-19 18:33:07 +0900

    create Today project

---
+++ Today/Today/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by 横山 祥平 on 2016/12/19.
+//  Copyright © 2016年 com.shoheiyokoyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
