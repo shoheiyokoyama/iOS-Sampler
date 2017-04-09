b925a12184dd6ae2b4987b7fa4be53644aed50e3
shoheiyokoyama <shohei.yok0602@gmail.com>
2017-04-10 08:55:38 +0900

    add test

--- Lock/Lock/ViewController.swift
+++ Lock/Lock/ViewController.swift
@@ -19,6 +19,10 @@ class ViewController: UIViewController {
         syncTest()
         semaphoreTest()
         nsLockTest()
+        nsLockErrorTest()
+        nsRecursiveLockTest()
+        unfairLockTest()
+        objcSyncTest()
     }
     
     func check(expect: Int = 10000) -> Bool {
@@ -70,6 +74,23 @@ extension ViewController {
         let result = check()
         // result is true
         print(result)
+        
+        var _num: Int = 0
+        var num: Int {
+            get {
+                return lockQueue.sync { _num }
+            }
+            set {
+                lockQueue.sync { _num = newValue }
+            }
+        }
+        
+        // deadlock
+        /*
+        lockQueue.sync {
+            num += 1
+        }
+         */
     }
     
     // MARK: - DispatchSemaphore
@@ -100,6 +121,101 @@ extension ViewController {
             nsLock.lock()
             self?.number += 1
             nsLock.unlock()
+        }
+        
+        sleep(1)
+        
+        let result = check()
+        // result is true
+        print(result)
+    }
+    
+    func nsLockErrorTest() {
+        let lock = NSLock()
+        
+        var _number: Int = 3
+        var number: Int {
+            get {
+                lock.lock(); defer { lock.unlock() }
+                return _number
+            }
+            set {
+                lock.lock(); defer { lock.unlock() }
+                _number = newValue
+            }
+        }
+        
+        lock.lock()
+        // Occur NSLockError because of recursive lock
+        print(number)//*** -[NSLock lock]: deadlock (<NSLock: 0x6080000dabe0> '(null)') *** Break on _NSLockError() to debug
+        lock.unlock()
+    }
+    
+    // MARK: - NSRecursiveLock
+    func nsRecursiveLockTest() {
+        defer { clean() }
+        
+        let lock = NSRecursiveLock()
+        excuteOnSeparateThread { [weak self] in
+            lock.lock()
+            self?.number += 1
+            lock.unlock()
+        }
+        
+        sleep(1)
+        
+        let result = check()
+        // result is true
+        print(result)
+        
+        clean()
+        
+        var _number: Int = 3
+        var number: Int {
+            get {
+                lock.lock(); defer { lock.unlock() }
+                return _number
+            }
+            set {
+                lock.lock(); defer { lock.unlock() }
+                _number = newValue
+            }
+        }
+        
+        
+        lock.lock()
+        // don't Occur NSLockError
+        let _ = number
+        lock.unlock()
+    }
+    
+    func unfairLockTest() {
+        defer { clean() }
+        
+        var lock = os_unfair_lock_s()
+        excuteOnSeparateThread { [weak self] in
+            os_unfair_lock_lock(&lock)
+            self?.number += 1
+            os_unfair_lock_unlock(&lock)
+        }
+        
+        sleep(1)
+        
+        let result = check()
+        // result is true
+        print(result)
+    }
+    
+    func objcSyncTest() {
+        defer { clean() }
+        
+        excuteOnSeparateThread { [weak self] in
+            // obj is key 
+            // recursive lock can't work, in the case same thread and key
+            // same as @synchronized
+            objc_sync_enter(self)
+            self?.number += 1
+            objc_sync_exit(self)
         }
         
         sleep(1)
