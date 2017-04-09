229fef64f896e7dda2a78cd231c00baefcf1d958
shoheiyokoyama <shohei.yok0602@gmail.com>
2017-04-09 16:51:06 +0900

    sync test

--- Lock/Lock/ViewController.swift
+++ Lock/Lock/ViewController.swift
@@ -9,11 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let lockQueue = DispatchQueue(label: "serialQueue")
+    var numbers: [Int] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // MARK: - sync
+        (0..<1000).forEach { index in
+            lockQueue.sync {
+                numbers.append(index)
+            }
+        }
+        
+        sleep(1)
+        
+        var result = check()
+        // result is true
+        print(result)
+        
+        (0..<1000).forEach { index in
+            numbers.append(index)
+        }
+        
+        sleep(1)
+        
+        result = check()
+        // result is false
+        print(result)
     }
     
+    func check() -> Bool {
+        var result = true
+        numbers.enumerated().forEach { index, num in
+            if num != index {
+                result = false
+            }
+        }
+        return result
+    }
 }
+
 
