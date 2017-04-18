cff4250cf99a6d53e9ab8cb53fc8002124e71ea1
shoheiyokoyama <shohei.yok0602@gmail.com>
2017-04-18 09:35:53 +0900

    Add comment for Do

--- GCD/GCD/SerialTask.swift
+++ GCD/GCD/SerialTask.swift
@@ -18,13 +18,13 @@ protocol Serializable {
     associatedtype ElementType
     
     init(_ closure: @escaping (@escaping (ElementType?) -> Void, @escaping (Error?) -> Void) -> Void)
-    //func fmap<Result>(_ transform: @escaping (ElementType) -> Result) -> SerialTask<Result>
+    func fmap<Result>(_ transform: @escaping (ElementType) -> Result) -> SerialTask<Result>
 }
 
 //asyncAfter
 //抽象クラス　継承して各operatorクラスを作る
 
-final class SerialTask<Element>: Serializable {
+class SerialTask<Element>: Serializable {
     
     typealias ElementType = Element
     
@@ -40,7 +40,7 @@ final class SerialTask<Element>: Serializable {
     fileprivate var nextFullfillHandler: ((Element?) -> Void)?
     fileprivate var nextErrorHandler: ((Error?) -> Void)?
     
-    init(_ closure: @escaping InitialTask) {
+    required init(_ closure: @escaping InitialTask) {
         excuteTask(closure)
     }
     
@@ -67,7 +67,7 @@ final class SerialTask<Element>: Serializable {
     
     func excuteTask(_ task: InitialTask) {
         //weak self にすると解放されてしまう
-        let fulfill: Fullfill = {  value in
+        let fulfill: Fullfill = { value in
             self.nextFullfillHandler?(value)
         }
         
@@ -91,10 +91,10 @@ extension SerialTask {
     // Mapのインスタンスを返す設計にすればMap operatorのインタフェースが明確にできる
     //http://jutememo.blogspot.jp/2008/10/haskell-fmap.html
     func fmap<Result>(_ transform: @escaping (Element) -> Result) -> SerialTask<Result> {
-        // return Fmap<NewElement>() .....
         
-        
-        return SerialTask<Result> { [weak self] fullfill, error in
+        //今の所Fmap使わなくてもいいかも
+        // nextFullfillHandlerはFmapがもつべき
+        return Fmap<Result> { [weak self] fullfill, error in
             //let _ = ConcurrentTask<Element2>(value: newValue)
             
             guard let me = self else { return }
@@ -114,12 +114,40 @@ extension SerialTask {
         }
     }
     
-    //引数がある場合も
+    
     // convetible準拠させる
-    /*
-    func `do`() -> Convertible {
-        return
-    }*/
+    // 帰り値なし
+    // - 引数あり
+    // - errorあり
+    // - 引数なし
+    // - fullfill
+    
+    
+    //何も引数を受け取らずに何も返さないDo
+    //valueを捨てること許容する？do onNextみたいにvalueは保持して次に流す
+    func `do`(_ completionHandler: @escaping () -> Void) -> Do<Element> {
+        return Do<Element> { [weak self] fullfill, error in
+            //let _ = ConcurrentTask<Element2>(value: newValue)
+            
+            guard let me = self else { return }
+            
+            let next: (Element) -> Void = { value in
+                completionHandler()
+                fullfill()//value
+            }
+            
+            let failure: (Error?) -> Void = { errorInfo in
+                error(errorInfo)//初期化時のfailue実行
+            }
+            
+            me.nextFullfillHandler = next
+            me.nextErrorHandler    = failure
+        }
+    }
+    
+    func doWith() {
+        
+    }
     
     //fmapWith
 }
@@ -139,25 +167,39 @@ extension SerialTask: ErrorCatchable {
 //protocol Functor { }
 
 //TODO: - serialize ErrorCatchableなど継承 convertibleは準拠させない
-final class Fmap<Element>: Serializable {
+final class Fmap<Element>: SerialTask<Element> {
     
-    // - stateどうするか
-    // - 継承すべきか
-    
-    typealias ElementType =  Element
-    //task
-    typealias Fullfill = (ElementType?) -> Void
-    typealias Failure  = (Error?) -> Void
-    typealias InitialTask = (@escaping Fullfill, @escaping Failure) -> Void
+    required init(_ closure: @escaping InitialTask) {
+        super.init(closure)
+    }
+}
+
+//valueは内部的に次に流す（rxのDoと同じ）
+final class Do<Element> {
+    //completehandler
+    typealias InitialTask = (@escaping () -> Void, @escaping (Error?) -> Void) -> Void
     
     init(_ closure: @escaping InitialTask) {
-        //excuteTask(closure)
+        excuteTask(closure)
     }
     
-    /*
-    func fmap<Result>(_ transform: @escaping (Element) -> Result) -> SerialTask<Result> {
-        return SerialTask<Result>()
-    }*/
+    func excuteTask(_ task: InitialTask) {
+        //weak self にすると解放されてしまう
+        let fulfill: () -> Void = { value in
+            
+        }
+        
+        let failure: (Error?) -> Void = {  error in
+            // errorを非optionalにしてもいいかも
+            if let handler = self.errorHandler, let error = error {
+                handler(error)
+            } else {
+                self.nextErrorHandler?(error)
+            }
+        }
+        
+        task(fulfill, failure)
+    }
 }
 
 //TODO: -
