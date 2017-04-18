9b33a33714d45905fb56d80c66ce1d2235b7eaee
shoheiyokoyama <shohei.yok0602@gmail.com>
2017-04-18 10:39:01 +0900

    Make TextViewSample

---
+++ TextViewSample/TextViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextViewSample
+//
+//  Created by 横山 祥平 on 2017/04/18.
+//  Copyright © 2017年 Shohei Yokoyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
