5184e34855aa51dc4943be01ebe85229c387774b
shoheiyokoyama <shohei.yok0602@gmail.com>
2017-05-10 16:45:07 +0900

    Create LocalPhotoViewer

---
+++ LocalPhotoViewer/LocalPhotoViewer/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  LocalPhotoViewer
+//
+//  Created by 横山 祥平 on 2017/05/10.
+//  Copyright © 2017年 Shohei Yokoyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
