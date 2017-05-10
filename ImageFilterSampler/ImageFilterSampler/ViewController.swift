c904d7c7d83b8cc15b1cc254eda1c60f17800e0b
shoheiyokoyama <shohei.yok0602@gmail.com>
2017-05-10 14:26:15 +0900

    Change image filter by using slider

--- ImageFilterSampler/ImageFilterSampler/ViewController.swift
+++ ImageFilterSampler/ImageFilterSampler/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+//https://developer.apple.com/library/content/documentation/GraphicsImaging/Reference/CoreImageFilterReference/
 class ViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
@@ -21,6 +22,14 @@ class ViewController: UIViewController {
     
     //for peformance
     var ciContext = CIContext(options: nil)
+    
+    @IBOutlet weak var contrastSlider: UISlider!
+    @IBOutlet weak var brightnessSlider: UISlider!
+    @IBOutlet weak var saturationSlider: UISlider!
+    
+    let saturationFilter = CIFilter(name: "CIColorControls")
+    let brightnessFilter = CIFilter(name: "CIColorControls")
+    let constrastFilter  = CIFilter(name: "CIColorControls")
     
     var filterNames = [
         "CIPhotoEffectChrome",
@@ -40,6 +49,36 @@ class ViewController: UIViewController {
         collectionView.register(nib, forCellWithReuseIdentifier: "ImageCollectionViewCell")
         collectionView.delegate = self
         collectionView.dataSource = self
+    }
+    
+    @IBAction func changeSaturation(_ sender: Any) {
+        guard let sender = sender as? UISlider else { return }
+        saturationFilter?.setValue(NSNumber(value:sender.value), forKey: kCIInputSaturationKey)
+        
+        saturationFilter?.setValue(coreImage, forKey: kCIInputImageKey)
+        let filteredImageData = saturationFilter!.outputImage
+        let filteredImageRef = ciContext.createCGImage(filteredImageData!, from: (filteredImageData?.extent)!)
+        imageView.image = UIImage(cgImage: filteredImageRef!)
+    }
+    
+    @IBAction func changedBrightness(_ sender: Any) {
+        guard let sender = sender as? UISlider else { return }
+        brightnessFilter?.setValue(NSNumber(value:sender.value), forKey: kCIInputBrightnessKey)
+        
+        brightnessFilter?.setValue(coreImage, forKey: kCIInputImageKey)
+        let filteredImageData = brightnessFilter!.outputImage
+        let filteredImageRef = ciContext.createCGImage(filteredImageData!, from: (filteredImageData?.extent)!)
+        imageView.image = UIImage(cgImage: filteredImageRef!)
+    }
+    
+    @IBAction func changedConstrast(_ sender: Any) {
+        guard let sender = sender as? UISlider else { return }
+        constrastFilter?.setValue(NSNumber(value:sender.value), forKey: kCIInputContrastKey)
+        
+        constrastFilter?.setValue(coreImage, forKey: kCIInputImageKey)
+        let filteredImageData = constrastFilter!.outputImage
+        let filteredImageRef = ciContext.createCGImage(filteredImageData!, from: (filteredImageData?.extent)!)
+        imageView.image = UIImage(cgImage: filteredImageRef!)
     }
 }
 
