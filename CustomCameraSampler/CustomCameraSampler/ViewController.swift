c3a22a683baf3e4c367409d9695c5a56b654828c
shoheiyokoyama <shohei.yok0602@gmail.com>
2017-05-10 16:27:35 +0900

    Create camera sampler

---
+++ CustomCameraSampler/CustomCameraSampler/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CustomCameraSampler
+//
+//  Created by 横山 祥平 on 2017/05/10.
+//  Copyright © 2017年 Shohei Yokoyama. All rights reserved.
+//
+
+import UIKit
+
+//http://qiita.com/tfutada/items/3e415cbe176d6f801b1d
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
