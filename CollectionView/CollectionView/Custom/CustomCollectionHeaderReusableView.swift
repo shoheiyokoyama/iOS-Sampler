5b1b172b0d6396b4d68a903d722e7d2bc65a8439
shoheiyokoyama <shohei.yok0602@gmail.com>
2017-05-11 17:58:50 +0900

    Set header collection view cell

---
+++ CollectionView/CollectionView/Custom/CustomCollectionHeaderReusableView.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCollectionHeaderReusableView.swift
+//  CollectionView
+//
+//  Created by 横山 祥平 on 2017/05/11.
+//  Copyright © 2017年 Shohei Yokoyama. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionHeaderReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+    }
+}
--- CollectionView/CollectionView/Custom/CustomController.swift
+++ CollectionView/CollectionView/Custom/CustomController.swift
@@ -11,6 +11,7 @@ import UIKit
 class CustomController: UIViewController {
     
     let cellIdentifier = "CustomCell"
+    let headerIdentifier = "CustomCollectionHeaderReusableView"
 
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -22,6 +23,9 @@ class CustomController: UIViewController {
         
         collectionView.dataSource = self
         collectionView.delegate = self
+        
+        let headerNib = UINib(nibName: headerIdentifier, bundle: nil)
+        collectionView.register(headerNib, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerIdentifier)
     }
 }
 
@@ -31,16 +35,23 @@ extension CustomController: UICollectionViewDelegate {
 
 extension CustomController: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
+        return 2
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 100
+        return 5
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath)
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "CustomCollectionHeaderReusableView", for: indexPath) as? CustomCollectionHeaderReusableView else { return .init() }
+        
+        headerView.backgroundColor = UIColor.white
+        return headerView
     }
 }
 
@@ -56,6 +67,13 @@ extension CustomController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         let margin: CGFloat = 5
         return UIEdgeInsets(top: 0, left: 0, bottom: margin, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        if section == 0 {
+            return CGSize(width: 300, height: 50)
+        }
+        return .zero
     }
 }
 
