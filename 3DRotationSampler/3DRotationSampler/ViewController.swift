4a14075388bec587ee5fa040f2c4dfa45d21906e
shoheiyokoyama <shohei.yok0602@gmail.com>
2017-05-20 18:27:23 +0900

    Set anchorPoint

--- 3DRotationSampler/3DRotationSampler/ViewController.swift
+++ 3DRotationSampler/3DRotationSampler/ViewController.swift
@@ -33,6 +33,28 @@ class ViewController: UIViewController {
         let degree = CGFloat(slider.value) * 180
         let radian = degree * CGFloat.pi / 180
         imageView.layer.transform = CATransform3DMakeRotation(radian, 0, 1, 0)
+        //setAnchorPoint(anchorPoint: CGPoint(x: 0, y: 0.5), forView: imageView)
+        setAnchorPoint(anchorPoint: CGPoint(x: 1, y: 0.5), forView: imageView)
+    }
+    
+    //http://qiita.com/usagimaru/items/b085d13ac47707ce5c69
+    func setAnchorPoint(anchorPoint: CGPoint, forView view: UIView) {
+        var newPoint = CGPoint(x: view.bounds.size.width * anchorPoint.x, y: view.bounds.size.height * anchorPoint.y)
+        var oldPoint = CGPoint(x: view.bounds.size.width * view.layer.anchorPoint.x, y: view.bounds.size.height * view.layer.anchorPoint.y)
+        
+        
+        newPoint = newPoint.applying(view.transform)
+        oldPoint = oldPoint.applying(view.transform)
+        
+        var position = view.layer.position
+        position.x -= oldPoint.x
+        position.x += newPoint.x
+        
+        position.y -= oldPoint.y
+        position.y += newPoint.y
+        
+        view.layer.position = position
+        view.layer.anchorPoint = anchorPoint
     }
 }
 
